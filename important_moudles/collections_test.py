924060a702cc81dac4a295863b6443b789050ea1
Chiens <bloodchildren@163.com>
2017-04-10 20:56:36 +0800

    这个是模块collections的练习文件

---
+++ important_moudles/collections_test.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+
+'''这是python内建模块collections的练习文件，主要用于集合
+    此处会练习的内容有：
+    1.namedtuple(typename, field_names, verbose=False, rename=False)
+    2.deque,实际上deque是来自_collections.py的一个类，从源码来看它就是双端队列
+    3.defaultdict,它同样来自_collections.py的一个类，defaultdict(dict)
+    4.OrderedDict(dict),它在位置在collecitons/__init__.py
+    5.Counter(dict)类
+'''
+
+from collections import *
+
+#
