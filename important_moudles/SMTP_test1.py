c84fdb8662d93718de08467a01f51510468a06ff
Chiens <bloodchildren@163.com>
2017-04-22 11:15:47 +0800

    SMTP邮件发送，带头部文件

---
+++ important_moudles/SMTP_test1.py
@@ -0,0 +1,37 @@
+#!usr/bin/env python
+#encoding:utf-8
+'''
+@version:python3.4
+@author:Chiens
+带头部文件的邮件
+'''
+from email import encoders
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+
+import smtplib
+
+#格式化邮件地址
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    #返回formataddr对象，其参数是一个元组，里面的name要经过Header编码
+    return formataddr((Header(name, 'utf-8').encode(), addr))
+
+from_addr = input("From:")
+password = input("Password:")
+to_addr = input("To:")
+smtp_server = input("SMTP server:")
+
+msg = MIMEText("Hello, send by Python...", 'plain', 'utf-8')
+#添加邮件头
+msg['From'] = _format_addr("Python爱好者<%s>" % from_addr)
+msg['To'] = _format_addr("管理员<%s>" % to_addr)
+msg['Subject'] = Header("来自SMTP的问候...",'utf-8').encode()
+
+
+server = smtplib.SMTP(smtp_server, 25)
+server.set_debuglevel(1) #显示和SMTP服务器交互的信息
+server.login(from_addr, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()
